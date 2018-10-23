6005122399ebf6b51c54ca3bfd0f635d7aa76683
Emad Aghayi <emad.aghayi@gmail.com>
2018-10-23 19:25:03 -0400

    finalize test of fetchTodo endpoints

--- app.js
+++ app.js
@@ -14,13 +14,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
